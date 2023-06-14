356ee99d0d191f740cd696c2ad90858dadc6b24f
Kyle Essex <kylegavinessex@hotmail.com>
2023-06-14 22:16:07 +0100

    build

--- dist/index.es.js
+++ dist/index.es.js
@@ -3,32 +3,34 @@ import eslint from 'gulp-eslint-new';
 
 function lintTask(src, opts) {
     if (opts === void 0) { opts = null; }
-    // Default options
-    var defaultOpts = {
-        eslint: {
-            baseConfig: {
-                extends: ['eslint:recommended'],
-                env: {
-                    browser: true,
-                    node: true,
-                    es6: true
-                },
-                parserOptions: {
-                    ecmaVersion: 7,
-                    ecmaFeatures: {
-                        jsx: true,
+    return function () {
+        // Default options
+        var defaultOpts = {
+            eslint: {
+                baseConfig: {
+                    extends: ['eslint:recommended'],
+                    env: {
+                        browser: true,
+                        node: true,
+                        es6: true
+                    },
+                    parserOptions: {
+                        ecmaVersion: 7,
+                        ecmaFeatures: {
+                            jsx: true,
+                        }
                     }
                 }
             }
-        }
+        };
+        // Merge default options with user options
+        opts = Object.assign({}, defaultOpts, opts);
+        // Run ESLint
+        return gulp.src(src)
+            .pipe(eslint(opts.eslint))
+            .pipe(eslint.format())
+            .pipe(eslint.failAfterError());
     };
-    // Merge default options with user options
-    opts = Object.assign({}, defaultOpts, opts);
-    // Run ESLint
-    return gulp.src(src)
-        .pipe(eslint(opts.eslint))
-        .pipe(eslint.format())
-        .pipe(eslint.failAfterError());
 }
 
 export { lintTask };
