b8e454a5d84ed4af40ef1a378f7d5144d5633e1e
Kyle Essex <kylegavinessex@hotmail.com>
2023-06-14 22:13:43 +0100

    return function

--- src/index.ts
+++ src/index.ts
@@ -20,33 +20,35 @@ interface lintOptions {
   eslint: GulpESLintOptions;
 }
 
-export function lintTask(src: string, opts: lintOptions = null): NodeJS.ReadWriteStream {
-  // Default options
-  const defaultOpts: lintOptions = {
-    eslint: {
-      baseConfig: {
-        extends: ['eslint:recommended'],
-        env: {
-          browser: true,
-          node: true,
-          es6: true
-        },
-        parserOptions: {
-          ecmaVersion: 7,
-          ecmaFeatures: {
-            jsx: true,
+export function lintTask(src: string, opts: lintOptions = null): () => NodeJS.ReadWriteStream {
+  return function () {
+    // Default options
+    const defaultOpts: lintOptions = {
+      eslint: {
+        baseConfig: {
+          extends: ['eslint:recommended'],
+          env: {
+            browser: true,
+            node: true,
+            es6: true
+          },
+          parserOptions: {
+            ecmaVersion: 7,
+            ecmaFeatures: {
+              jsx: true,
+            }
           }
         }
       }
     }
+
+    // Merge default options with user options
+    opts = Object.assign({}, defaultOpts, opts);
+
+    // Run ESLint
+    return gulp.src(src)
+      .pipe(eslint(opts.eslint))
+      .pipe(eslint.format())
+      .pipe(eslint.failAfterError());
   }
-
-  // Merge default options with user options
-  opts = Object.assign({}, defaultOpts, opts);
-
-  // Run ESLint
-  return gulp.src(src)
-    .pipe(eslint(opts.eslint))
-    .pipe(eslint.format())
-    .pipe(eslint.failAfterError());
 }
